3e7b3fa917af86bcd048f215ff7dc24b2a63d9c3
carrliitos <salazar.ten80@gmail.com>
2020-11-13 19:47:34 -0600

    having import troubles again

---
+++ snake/algorithms/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""AI algorithms package."""
+
+from path import PathAlgorithm
+from greedy import GreedyAlgorithm
+from hamilton import HamiltonAlgorithm
+from dqn import DQNAlgorithm
---
+++ snake/algorithms/greedy.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# pylint: disable=C0111
+
+from snake.base.pos import Pos
+from snake.solver.base import BaseAlgorithm
+from snake.solver.path import PathAlgorithm
+
+class GreedyAlgorithm(BaseAlgorithm):
+	def __init__(self, snake):
+		super().__init__(snake)
+		self._pathSolver = PathAlgorithm(snake)
