f1e392298d298d72f685c31b4087c27eb9fd57b1
carrliitos <salazar.ten80@gmail.com>
2020-11-25 23:02:53 -0600

    updates

--- snake/algorithms/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-
-"""AI algorithms package."""
-
-from path import PathAlgorithm
-from greedy import GreedyAlgorithm
-from hamilton import HamiltonAlgorithm
-from dqn import DQNAlgorithm
